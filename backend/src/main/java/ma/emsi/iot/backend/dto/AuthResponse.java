9a6814ae105bebdc85bbd0299fdac18a24877360
mouad45-bit <mouadfekkak2002@gmail.com>
2026-05-01 19:49:57 +0100

    create /backend/dto/AuthResponse.java

---
+++ backend/src/main/java/ma/emsi/iot/backend/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package ma.emsi.iot.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String username;
+    private String role;
+}
