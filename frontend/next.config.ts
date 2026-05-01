14b8f8e577fc3b3a1f441f1aeac83a58b406fcac
mouad45-bit <mouadfekkak2002@gmail.com>
2026-05-01 23:20:10 +0100

    add allowedDevOrigins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    allowedDevOrigins: ["192.168.119.131", "localhost"],
 };
 
 export default nextConfig;
