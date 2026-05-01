c40988794adf05e4917b068d644edbc32ee72684
mouad45-bit <mouadfekkak2002@gmail.com>
2026-05-01 19:49:04 +0100

    create /backend/dto/RegisterRequest.java

---
+++ backend/src/main/java/ma/emsi/iot/backend/dto/RegisterRequest.java
@@ -0,0 +1,10 @@
+package ma.emsi.iot.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+}
